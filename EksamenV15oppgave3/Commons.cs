490f79041684a356b6a82f22c283f3623afac071
Andreas Valen <133861@stud.hvl.no>
2018-11-21 17:51:16 +0100

    Even Ligård

---
+++ EksamenV15oppgave3/Commons.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamenV15oppgave3 //Husk å endre!
+{
+    public static class Commons
+    {
+        /// <summary>
+        /// Spør etter en gylidig int. Godtar ikke variablen før brukeren taster inn en int.
+        /// </summary>
+        public static int GetIntConsole(string feilmelding = "", int minVerdi = Int32.MinValue, int maksVerdi = Int32.MaxValue)
+        {
+            int i;
+            while (!(int.TryParse(Console.ReadLine(), out i)))
+            {
+                if (feilmelding.Length > 0)
+                    Console.WriteLine(feilmelding);
+            }
+            if (i < minVerdi || i > maksVerdi)
+
+            {
+                Console.WriteLine($"Verdien er ikke innenfor grensen, skriv inn en verdi mellom {minVerdi} og {maksVerdi}");
+                return GetIntConsole(feilmelding, minVerdi, maksVerdi);
+            }
+            return i;
+        }
+        /// <summary>
+        /// Skriver en streng til en fil
+        /// </summary>
+        public static void SkrivTilFil(string filplassering, string tekstTilFil, bool append = false)
+        {
+            if (append)
+            {
+                using (System.IO.StreamWriter sw = System.IO.File.AppendText(filplassering))
+                {
+                    sw.WriteLine(tekstTilFil);
+                }
+            }
+            else
+            {
+                string[] tekst = new string[] {tekstTilFil};
+                System.IO.File.WriteAllLines(filplassering, tekst);
+            }
+        }
+        /// <summary>
+        /// Skriver et array til en fil
+        /// </summary>
+        public static void SkrivTilFil(string filplassering, string[] tekstTilFil, bool append = false)
+        {
+            if (append)
+            {
+                using (System.IO.StreamWriter sw = System.IO.File.AppendText(filplassering))
+                {
+                    foreach (string linje in tekstTilFil)
+                    {
+                        sw.WriteLine(linje);
+                    }
+                }
+            }
+            else
+            {
+                System.IO.File.WriteAllLines(filplassering, tekstTilFil);
+            }
+        }
+        /// <summary>
+        /// Leser fra en fil og returnerer ny array med innholdet
+        /// </summary>
+        public static string[] LesFraFil(string filplassering, int fjernOverskriftLinjer = 0)
+        {
+            try
+            {
+                string[] tekstlinjerFraFil = System.IO.File.ReadAllLines(filplassering);
+                if (fjernOverskriftLinjer != 0)
+                {
+                    Array.Copy(tekstlinjerFraFil,fjernOverskriftLinjer, (tekstlinjerFraFil=new string[tekstlinjerFraFil.Length- fjernOverskriftLinjer]),0,tekstlinjerFraFil.Length);
+                }
+                return tekstlinjerFraFil;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return new string[] {$"Fant ikke filen {filplassering}"};
+            }
+        }
+    }
+}
---
+++ EksamenV15oppgave3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamenV15oppgave3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> linjerFraFil = new List<string>(Commons.LesFraFil("oppg3-data.txt", 2));
+            string [] snitt = new string[linjerFraFil.Count];
+            Console.WriteLine("Antall linjer lest: " + linjerFraFil.Count);
+            int i = 0;
+            foreach(string linje in linjerFraFil)
+            {
+                string[] verdier = linje.Split(' ');
+                double verdi1 = Convert.ToDouble(verdier[0]);
+                double verdi2 = Convert.ToDouble(verdier[1]);
+                double gjennomsnitt = (verdi1 + verdi2) / 2;
+                double delta = verdi1 - verdi2;//studasskommentar: Fungerer nå, men ikke hvis verdi1 er positiv og verdi2 er negativ
+                if (Math.Abs(delta) > 2)
+                {
+                    Console.WriteLine($"Verdier på linje {i} har differanse større enn 2. Differansen er {delta:F3}");
+                }
+                snitt[i]=Convert.ToString(gjennomsnitt);
+                i++;
+            }
+            double sum = 0;
+            foreach(string tall in snitt)
+            {
+                double verdi = Convert.ToDouble(tall);
+                sum += verdi;
+                
+            }
+            double totalgjennomsnitt = sum / 20;
+            double max = 0;
+            double min = 100;
+            foreach(string s in snitt)
+            {
+                double v = Convert.ToDouble(s);
+                if (v > max)
+                {
+                    max = v;
+                }
+                else if (v < min)
+                {
+                    min = v;
+                }
+
+            }
+            Console.WriteLine("Totalt gjennomsnitt: "+totalgjennomsnitt);
+            Console.WriteLine("Maksimumverdi: " +max);
+            Console.WriteLine("Minimumverdi: "+min);
+            Commons.SkrivTilFil("oppg3-rapport.txt", snitt, false);
+            Console.ReadKey();
+        }
+    }
+}
